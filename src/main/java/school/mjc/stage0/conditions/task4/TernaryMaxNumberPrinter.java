53fd2dfdbc4b4e9ba6782f9296b2d3999aa19212
Zotoff <paulzotoff@gmail.com>
2023-06-11 19:17:09 +0300

    First attempt

--- src/main/java/school/mjc/stage0/conditions/task4/TernaryMaxNumberPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task4/TernaryMaxNumberPrinter.java
@@ -2,5 +2,14 @@ package school.mjc.stage0.conditions.task4;
 
 public class TernaryMaxNumberPrinter {
     public void printGreatest(int first, int second, int third) {
+        int biggest = first > second ? (first > third ? first : third) : (second > third ? second : third);
+        System.out.println(biggest);
+        if (biggest == first && first == second) {
+            System.out.println(first);
+        } else if (biggest == first && first == third) {
+            System.out.println(third);
+        } else if (biggest == second && second == third) {
+            System.out.println(second);
+        }
     }
 }
